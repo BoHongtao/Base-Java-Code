116f96a69826a792b9d3e1e20ec33d2c2cbc5428
BoHongtao <BoHongtao@yeah.net>
2018-11-22 17:05:56 +0800

    login and return userInfo

--- javaEE/webDemo/firstweb/src/cn/phoenix_tree/service/UserService.java
+++ javaEE/webDemo/firstweb/src/cn/phoenix_tree/service/UserService.java
@@ -1,9 +1,11 @@
 package cn.phoenix_tree.service;
 
+import cn.phoenix_tree.dao.UserDao;
 import cn.phoenix_tree.domain.User;
 
 public class UserService {
-    public boolean login(User user){
-        return true;
+    UserDao dao = new UserDao();
+    public User login(User user){
+        return dao.login(user);
     }
 }
--- javaEE/webDemo/firstweb/src/cn/phoenix_tree/servlet/LoginServlet.java
+++ javaEE/webDemo/firstweb/src/cn/phoenix_tree/servlet/LoginServlet.java
@@ -1,8 +1,8 @@
 package cn.phoenix_tree.servlet;
 
 import cn.phoenix_tree.domain.User;
+import cn.phoenix_tree.service.UserService;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,22 +11,15 @@ import java.io.IOException;
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws IOException {
         request.setCharacterEncoding("UTF-8");
         String userName = request.getParameter("username");
         String pwd = request.getParameter("pwd");
-//        User user = new User();
-//        user.setPwd(pwd);
-//        user.setUsername(userName);
         System.out.println("get请求,获得的参数是："+userName+",pwd:"+pwd);
     }
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        /*
-         *  开发中我们发现，doget和dopost中的方法编写相同
-         *  所以我们经常在post中调用get
-         */
+            throws IOException {
         //doGet(request,response);
         request.setCharacterEncoding("UTF-8");
         String userName = request.getParameter("username");
@@ -34,6 +27,14 @@ public class LoginServlet extends HttpServlet {
         User user = new User();
         user.setPwd(pwd);
         user.setUsername(userName);
+        UserService userservice = new UserService();
+        //登陆获取用户信息
+        User res = userservice.login(user);
+        System.out.println("-------");
+        System.out.println(res.getUsername());
+        System.out.println(res.getPwd());
+        System.out.println(res.getId());
+        System.out.println(res.getSex());
         System.out.println("post请求,获得的参数是："+userName+",pwd:"+pwd);
     }
 }
